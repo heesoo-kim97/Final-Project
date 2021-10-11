4fe0901ae6049de05d5bec4f884ac6709d82bd18
heesoo-kim97 <heesoo0323@gmail.com>
2021-10-11 06:32:35 +0000

    added basic post request server.

--- server/index.js
+++ server/index.js
@@ -13,3 +13,11 @@ app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`express server listening on port ${process.env.PORT}`);
 });
+
+app.use(express.json());
+app.get('', (req, res) => {
+  const payload = 'hey there';
+  // eslint-disable-next-line no-console
+  console.log(req.body);
+  res.status(200).json(payload);
+});
